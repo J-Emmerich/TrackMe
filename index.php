486ddec7ccac4a65d448a34858842f39ed6c9060
Joao Emmerich <emmerichjoao@protonmail.com>
2021-03-18 07:05:14 +0100

    index.php now changes with session, it shows only 'logout' button when there is a session started, and signup/login when there is no sesssion

--- index.php
+++ index.php
@@ -19,30 +19,42 @@ include "header.php";
 
 <main>
 
-<div class="main-wrapper">
-<div class="form-wrapper">
-<h1>Sign Up</h1>
-<form action="includes/signup.inc.php" method="POST">
-<input type="text" name="username" placeholder="Choose your username">
-<input type="text" name="useremail" placeholder="what's your email?">
-<input type="password" name="userpwd" placeholder="Your Password Here">
-<input type="password" name="userpwd-repeat" placeholder="Repeat our password">
-<button type="submit" name="signup-submit">Sign Up</button>
-</form>
-</div>
-<div class="form-wrapper"></div>
-<h1>Login</h1>
-<form action="includes/signin.inc.php" method="POST">
-<input type="text" name="username" placeholder="Your Username">
-<input type="password" name="userpwd" placeholder="Your Password">
-<button type="submit" name="login-submit">LogIn</button>
-</form>
-</div>
-<div>
-    <form action="includes/logout.inc.php" method="POST">
-    <button type="submit" name="logout-submit">Logout</button>
-    </form>
-</div>
+    <?php
+    if(!isset($_SESSION['userId']))
+    {
+        echo '
+        <div class="main-wrapper">
+        <div class="form-wrapper">
+
+        <h1>Sign Up</h1>
+        <form action="includes/signup.inc.php" method="POST">
+        <input type="text" name="username" placeholder="Choose your username">
+        <input type="text" name="useremail" placeholder="whats your email?">
+        <input type="password" name="userpwd" placeholder="Your Password Here">
+        <input type="password" name="userpwd-repeat" placeholder="Repeat our password">
+        <button type="submit" name="signup-submit">Sign Up</button>
+        </form>
+        </div>
+        <div class="form-wrapper"></div>
+        <h1>Login</h1>
+        <form action="includes/signin.inc.php" method="POST">
+        <input type="text" name="username" placeholder="Your Username">
+        <input type="password" name="userpwd" placeholder="Your Password">
+        <button type="submit" name="login-submit">LogIn</button>
+        </form>
+        </div>
+';
+    }
+    else{
+ echo '
+        <div>
+            <form action="includes/logout.inc.php" method="POST">
+            <button type="submit" name="logout-submit">Logout</button>
+            </form>
+        </div>
+';
+    }
+    ?>
 
 </main>    
 </body>
